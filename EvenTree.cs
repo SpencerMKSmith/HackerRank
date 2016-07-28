1ad8518193fce8b0fd1faee6b960e34e715b64c1
Spencer Smith <smsmit35@gmail.com>
2016-07-27 18:25:52 -0700

    Optimized my Even Tree solution so that now it will calculate the number of descendants when it adds an edge to the graph, thus taking away the need to traverse the tree again to determine the number.

--- EvenTree.cs
+++ EvenTree.cs
@@ -5,40 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+/*
+	………. ,*˜*, ……. ,•°*•, Spencer MK Smith ,•*°•, …… ,*˜*, .……… 
+	„*˜……“• •“………'·,¸¸,‘•°. HackerRank .°•',¸¸,·'……..“• •“……˜*„
+	“‹„¸¸„ •°`'·.,¸¸,.·‘… Even Tree Solution …'·.,¸¸,.·'´°•„¸¸„›“
 
-//	………. ,*˜*, ……. ,•°*•, Spencer MK Smith ,•*°•, …… ,*˜*, .……… 
-//	„*˜……“• •“………'·,¸¸,‘•°. HackerRank .°•',¸¸,·'……..“• •“……˜*„
-//	“‹„¸¸„ •°`'·.,¸¸,.·‘… Even Tree Solution …'·.,¸¸,.·'´°•„¸¸„›“
-//
-// Problem can be found here: https://www.hackerrank.com/challenges/even-tree
-//
-//	Given a simply connected graph (a tree), we are asked to figure out the maximum
-//	number of edges that can be removed from the tree such that each resulting tree
-//	contains an even number of verticies.
-//
-//  Solution in a nut shell:  Given a tree of an even number of verticies, we know
-//	that we can remove an edge of the tree that connects to a subtree that contains 
-//	an even number of verticies as this will produce two even trees.  Keep in mind
-//	that the solution does not ask for which verticies will specifically will be
-//	removed, just for the number that can be removed.  This number can be found by
-//	determining how many subtrees exist with an even number of verticies.  This is
-//	done below by recursively traversing the tree and finding the number of descendants
-//	that each node has, the number of verticies that have an odd number of descendants
-//	is the answer to the problem.
+	This is my C# solution to Even Tree which can be found here: https://www.hackerrank.com/challenges/even-tree
+	
+	The solution is much easily understood when we understand this simple principle:
+			The number of edges we can remove = The number of even subtrees in the graph
+	
+	This is true because the tree that we are given as input will ALWAYS have an even number of nodes, therefore,
+		taking off any even number of nodes from it will still maintain the even number of nodes in each sub tree.
+		Now that we understand this principle, I now tell you that simply, the answer to this problem is the number
+		of nodes that has an even number of descendants.  This is true because if you remove the edge above a node
+		with an even number of descendants, it will be come an even subtree (because you include itself in the sub tree)
 
-namespace HREvenTree
-{
+*/
+	//Object used to each node in the tree
 	class Node
 	{
 		public int index;
-		public int parentNode;
-		public int numberOfDescendants;
+		public int parentNode = -1;
+		public int numberOfDescendants = 0;
 		public List<int> childNodes;
 
 		public Node( int aIndex )
 		{
 			index = aIndex;
-			parentNode = -1;
 			childNodes = new List<int>();
 		}
 	}
@@ -49,18 +43,19 @@ namespace HREvenTree
 
 		static void Main( string[] args )
 		{
-			int numberOfNodes, numberOfEdges, rootIndex = -1;
+			int numberOfNodes, numberOfEdges;
+			
+			//Read the input
 			string input = Console.ReadLine();
-
 			numberOfNodes = Convert.ToInt32( input.Substring( 0, input.IndexOf( " " ) ) );
 			numberOfEdges = Convert.ToInt32( input.Substring( input.IndexOf( " " ) + 1, ( input.Length - 1 ) - input.IndexOf( " " ) ) );
 
-			//Instantiate the array of nodes
+			//Create the array of nodes and instantiate them with their index
 			NodeList = new Node[numberOfNodes];
 			for( int i = 0; i < numberOfNodes; i++ )
-				NodeList[i] = new Node( i - 1 );
+				NodeList[i] = new Node( i );
 
-			//Read the input which defines the edges of the tree and store them in the NodeList structure
+			//Read the edge input, for each edge add
 			for( int i = 0; i < numberOfEdges; i++ )
 			{
 				string edgeDefinition = Console.ReadLine();
@@ -68,38 +63,24 @@ namespace HREvenTree
 				int to = Convert.ToInt32( edgeDefinition.Substring( 0, spaceIndex ) );
 				int from = Convert.ToInt32( edgeDefinition.Substring( spaceIndex + 1, edgeDefinition.Length - spaceIndex - 1 ) );
 
+				//Convert the input to the index in the array
 				NodeList[from - 1].childNodes.Add( to - 1 );
 				NodeList[to - 1].parentNode = from - 1;
+				
+				//Traverse up the tree and add one to number of descendants of all of the ancestors of the new node
+				for( int currentNodeIndex = (from-1); currentNodeIndex != -1; currentNodeIndex = NodeList[currentNodeIndex].parentNode )
+					NodeList[currentNodeIndex].numberOfDescendants++;
 			}
 
-			//I do not assume that the first node is the root, I look for the root node (the one that has no parent)
-			foreach(Node node in NodeList)
-				if( node.parentNode == -1 ) rootIndex = node.index;
-
-			getNumberOfDescendants( rootIndex );
-
-			//The answer to this problem is equal to the number of nodes that has an odd number of descendants
+			//The number of edges that we can remove is equal to the number of sub trees with an even number of nodes.
+			//This means that this number is equal to the number of nodes with an odd number of descendants because
+			//	a node with odd descendants + itself is always even.  So count the number of nodes with odd descendants and
+			//	return it
 			int answer = 0;
 			foreach( Node node in NodeList )
 				if( node.numberOfDescendants % 2 == 1 ) answer++;
 
-			//Print the answer (subtracted by 1 because the variable will count the root)
 			Console.WriteLine( answer - 1 );
 
 		}
-
-		//This recursive function will traverse the tree using depth first search and determine how many descendants each
-		//		node has.
-		public static int getNumberOfDescendants( int indexOfNode )
-		{
-			int descendants = 0;
-			for( int i = 0; i < NodeList[indexOfNode].childNodes.Count; i++ )
-				descendants += getNumberOfDescendants( NodeList[indexOfNode].childNodes[i] );
-
-			descendants += NodeList[indexOfNode].childNodes.Count;
-
-			NodeList[indexOfNode].numberOfDescendants = descendants;
-			return descendants;
-		}
 	}
-}
